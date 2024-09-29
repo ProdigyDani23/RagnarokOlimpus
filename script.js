3bd435763a0f885a3cb0b17dd36939b00a2d1557
Daniel Felipe Gómez Ferreira <pipeferreira1@gmail.com>
2024-09-29 08:04:43 -0500

    agregando mas rutinas

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ const routineData = {
                 repeticiones: "20 segundos",
                 peso:"-",
                 descanso:"sin descanso",
-                link:"pendiente"
+                link:"https://mundoentrenamiento.com/burpee-dice-la-ciencia/"
             },
             {
                 ejercicio:"Jump Squats",
@@ -187,7 +187,7 @@ const routineData = {
                 repeticiones: "20 segundos",
                 peso:"-",
                 descanso:"sin descanso",
-                link:"pendiente"
+                link:"https://infowod.com/jump-squats-construye-fuerza-potencia-y-agilidad/#:~:text=Jump%20squats%20son%20una%20variante,lo%20m%C3%A1s%20alto%20que%20puedas."
             },
             {
                 ejercicio:"Mountain climbers",
@@ -195,13 +195,82 @@ const routineData = {
                 repeticiones:"20 segundos",
                 peso:"-",
                 descanso:"1 minuto",
-                link:"pendiente"
+                link:"https://mundoentrenamiento.com/mountain-climbers/"
             }
-
+        ],
+        estiramiento: [
+            {
+                ejercicio: "Estiramiento dinámico de isquiotibiales (kicks frontales)",
+                series: "3",
+                repeticiones: "45 segundos",
+                peso: "-",
+                descanso:"sin descanso",
+                link:"https://www.youtube.com/watch?v=xHBufZX7Usw&ab_channel=HealthyFitness%7CEntrenadorPersonalyNutricionistaMadrid"
+            },
+            {
+                ejercicio:"Estiramiento de cuadrado lumbar",
+                series: "3",
+                repeticiones:"45 segundos por lado",
+                peso:"-",
+                descanso:"sin descanso",
+                link:"https://www.fisiobalance.cat/en/cuadrado-lumbar-como-estirarlo-eficazmente/"
+            }
         ]
        
-    }
+    },
+    thursday: {
+       title1:"Streching Bodytech",
+       descanso: [
+        {
+            ejercicio:"-",
+            series:"-",
+            repeticiones:"-",
+            peso:"-",
+            descanso:"-",
+            link:"-"
+        }
+       ]
+    },
+    friday: {
+        title1:"Abdomen (Resistencia y Fortalecimiento)",
+        title2:"Fortalecimiento de Lumbares",
+        title3:"Estiramientos y Flexibilidad",
+        abdomen: [
+            {
+                ejercicio: "Abs Coaster",
+                series:"4",
+                repeticiones:"20",
+                peso:"-",
+                descanso:"45 segundos",
+                link:"https://fitnessprogramer.com/exercise/ab-coaster-machine/"
+            },
+            {
+                ejercicio:"Rueda abdominal",
+                series:"4",
+                repeticiones:"15 repeticiones",
+                peso:"-",
+                descanso:"30-45 segundos",
+                link:"https://www.entrenamientos.com/ejercicios/abdominales-con-roller"
+            },
+            {
+                ejercicio:"Plancha Lateral con Rotación",
+                series:"4",
+                repeticiones:"45 segundos por lado",
+                peso:"-",
+                descanso:"30-45 segundos",
+                link:"https://www.youtube.com/watch?v=OGOxOkubYOc&ab_channel=ClubTenisPamplona"
+            },
+            {
+                ejercicio:"Plancha inversa",
+                series:"4",
+                repeticiones:"45 segundos",
+                peso:"-",
+                descanso:"30-45 segundos",
+                link:"https://www.myprotein.es/thezone/entrenamiento/plancha-invertida-isometrica/"
+            }
+        ]
 
+    },
 };
 
 // Función para cargar los títulos desde los datos del objeto routineData
@@ -250,6 +319,17 @@ document.getElementById('miercoles-tab').addEventListener('click', () =>{
     cargarTitulos('webnesday');
     cargarEjercicios('table1', routineData.webnesday.part1);
     cargarEjercicios('table2', routineData.webnesday.circuit);
-})
+    cargarEjercicios('table3', routineData.webnesday.estiramiento);
+});
 
+document.getElementById('jueves-tab').addEventListener('click', () => {
+cargarTitulos('thursday');
+cargarEjercicios('table1', routineData.thursday.descanso);
+cargarEjercicios('table2', routineData.thursday.descanso);
+cargarEjercicios('table3', routineData.thursday.descanso);
+});
 
+document.getElementById('viernes-tab').addEventListener('click', () => {
+cargarTitulos('friday');
+cargarEjercicios('table1', routineData.friday.abdomen);
+});
